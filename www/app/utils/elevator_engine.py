46beeff7cc598f7758898761edca985c82f3046f
Baptiste Lafontaine <baptiste33@gmail.com>
2013-10-22 14:02:39 +0200

    Adding a few debugger

--- www/app/utils/elevator_engine.py
+++ www/app/utils/elevator_engine.py
@@ -20,18 +20,22 @@ class ElevatorEngine:
     self._floorSelected = -1
   
   def call(self, atFloor, to):
+    web.debug('call at ' + atFloor + ' to floor ' + to)
     wp = WaitingPerson(atFloor,to)
     self._waitingList.put(wp)
     return
 	
   def go(self, floorToGo):
+    web.debug('go to floor' + floorToGo')
     self._floorSelected = floorToGo
     return
 	
   def userHasEntered(self):
+    web.debug('user has entered')
     self._numberOfPassenger += 1
 	
   def userHasExited(self):
+    web.debug('user has exited')
     self._numberOfPassenger -= 1
 	
   def reset(self, message):
@@ -39,8 +43,9 @@ class ElevatorEngine:
     self.__init__()
     
   def getNextCommand(self):
+    action = ElevatorEngine.ACTION_NOTHING
     if not self.isBusy and self.waitingList.empty(): #No one in the elevator or waiting for it
-      return ElevatorEngine.ACTION_NOTHING
+      action = ElevatorEngine.ACTION_NOTHING
       
     ###
     elif not self.isBusy and not self.waitingList.empty(): #Elevator empty but someone is waiting
@@ -50,52 +55,55 @@ class ElevatorEngine:
         if self.currentFloor < personToDeliver.waitingAtFloor:
           self._currentFloor = personToDeliver.waitingAtFloor
           self._currentCommand = ElevatorEngine.ACTION_UP
-          return ElevatorEngine.ACTION_UP
+          action = ElevatorEngine.ACTION_UP
         elif self.currentFloor > personToDeliver.waitingAtFloor:
           self._currentFloor = personToDeliver.waitingAtFloor
           self._currentCommand = ElevatorEngine.ACTION_DOWN
-          return ElevatorEngine.ACTION_DOWN
+          action = ElevatorEngine.ACTION_DOWN
         else:
           self._currentCommand = ElevatorEngine.ACTION_OPEN
-          return ElevatorEngine.ACTION_OPEN
+          action = ElevatorEngine.ACTION_OPEN
     
       elif self.currentCommand == ElevatorEngine.ACTION_UP or self.currentCommand == ElevatorEngine.ACTION_DOWN: #Was going to the right floor
         self._currentCommand = ElevatorEngine.ACTION_OPEN
-        return ElevatorEngine.ACTION_OPEN
+        action = ElevatorEngine.ACTION_OPEN
       
       elif self.currentCommand == ElevatorEngine.ACTION_OPEN: #Doors where open
         self._isBusy = True #Now the person is in the elevator
         self._currentCommand = ElevatorEngine.ACTION_CLOSE
-        return ElevatorEngine.ACTION_CLOSE
+        action = ElevatorEngine.ACTION_CLOSE
         
     ###   
     elif self.isBusy: #Elevator full
       if (self.currentCommand == ElevatorEngine.ACTION_CLOSE or self.currentCommand == ElevatorEngine.ACTION_NOTHING) and self.floorSelected == -1: #Doors are close, we are waiting the floor selection
         self._currentCommand = ElevatorEngine.ACTION_NOTHING
-        return ElevatorEngine.ACTION_NOTHING
+        action = ElevatorEngine.ACTION_NOTHING
       elif (self.currentCommand == ElevatorEngine.ACTION_CLOSE or self.currentCommand == ElevatorEngine.ACTION_NOTHING) and self.floorSelected != -1: # Floor selected
       
         #Go to the right floor
         if self.currentFloor < self.floorSelected:
           self._currentFloor = self.floorSelected
           self._currentCommand = ElevatorEngine.ACTION_UP
-          return ElevatorEngine.ACTION_UP
+          action = ElevatorEngine.ACTION_UP
         elif self.currentFloor > self.floorSelected:
           self._currentFloor = self.floorSelected
           self._currentCommand = ElevatorEngine.ACTION_DOWN
-          return ElevatorEngine.ACTION_DOWN
+          action = ElevatorEngine.ACTION_DOWN
         else:
           self._currentCommand = ElevatorEngine.ACTION_OPEN
-          return ElevatorEngine.ACTION_OPEN
+          action = ElevatorEngine.ACTION_OPEN
           
       elif self.currentCommand == ElevatorEngine.ACTION_UP or self.currentCommand == ElevatorEngine.ACTION_DOWN: #Was going to the right floor
         self._currentCommand = ElevatorEngine.ACTION_OPEN
-        return ElevatorEngine.ACTION_OPEN
+        action = ElevatorEngine.ACTION_OPEN
       
       elif self.currentCommand == ElevatorEngine.ACTION_NOTHING: #Doors are open, the person is gone
         self._currentCommand = ElevatorEngine.ACTION_NOTHING
         self._isBusy = False
-        return ElevatorEngine.ACTION_NOTHING
+        action = ElevatorEngine.ACTION_NOTHING
+        
+    web.debug('action: ' + action)
+    return action
     
   @property
   def currentFloor(self):
