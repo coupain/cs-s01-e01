10aad3a4cf368ffdd5bb6c5860d0cc5dad0a6a86
Baptiste Lafontaine <baptiste33@gmail.com>
2013-10-22 16:55:35 +0200

    wtf direction is not integer!

--- www/app/controllers/elevator.py
+++ www/app/controllers/elevator.py
@@ -14,7 +14,7 @@ class Elevator(object):
   def GET(self, action):
     params = web.input()
     if action == 'call':
-      elevator.call(int(params.atFloor),int(params.to))
+      elevator.call(int(params.atFloor),arams.to)
     elif action == 'go':
       elevator.go(int(params.floorToGo))
     elif action == 'userHasEntered':
