ad3e108e67fea5ac582fa7d52f8b91e6de6a1725
Baptiste Lafontaine <baptiste33@gmail.com>
2013-10-23 14:18:32 +0200

    Adding a few comments

--- www/app/utils/elevator_engine_baptiste.py
+++ www/app/utils/elevator_engine_baptiste.py
@@ -39,6 +39,7 @@ class ElevatorEngine:
     self._handleCall(floorToGo, ElevatorEngine.DIRECTION_NONE)
     return
 
+  """Counting the number of stop in the given direction"""
   def _numberOfStopsInDirection(self, direction):
     if direction == ElevatorEngine.DIRECTION_UP:
       intersect = self._stopsUp.union(self._stops).intersection(range(self._currentFloor, ElevatorEngine.FLOOR_MAX+1, 1))
@@ -53,14 +54,30 @@ class ElevatorEngine:
   def _openDoors(self):
     if self._doorsClosed == True:
       self._actions.put(ElevatorEngine.ACTION_OPEN)
-    self._handleStop(self._currentFloor)
+    self._handleStop(self._currentFloor) #Removing this floor from the lists
     self._doorsClosed = False
     
   def _closeDoors(self):
     if self._doorsClosed == False:
       self._actions.put(ElevatorEngine.ACTION_CLOSE)
       self._doorsClosed = True
+      
+  """Adding the floors where to stop in the lists"""
+  def _addStop(self, floor, direction):
+    web.debug("Adding stop to %d in direction %s" % (floor, direction))
     
+    #If floor is top or bottom, no matter the direction
+    if floor == ElevatorEngine.FLOOR_MAX or floor == 0:
+      direction = ElevatorEngine.DIRECTION_NONE
+    
+    if direction == ElevatorEngine.DIRECTION_UP:
+      self._stopsUp.add(floor)
+    elif direction == ElevatorEngine.DIRECTION_DOWN:
+      self._stopsDown.add(floor)
+    else:
+      self._stops.add(floor)
+    
+  """Remove the given floor from all stops lists"""
   def _handleStop(self, floor):
     if self._currentFloor in self._stopsUp:
       self._stopsUp.remove(floor)
@@ -124,38 +141,30 @@ class ElevatorEngine:
       self._goUp()
     
   def getNextCommand(self):
+    web.debug("current floor %d" % self._currentFloor)
     web.debug("stops up:" + " - ".join(str(s) for s in self._stopsUp))
     web.debug("stops down:" + " - ".join(str(s) for s in self._stopsDown))
     web.debug("stops:" + " - ".join(str(s) for s in self._stopsDown))
+    #if the current floor is in the stops no matter the firection
     if self._currentFloor in self._stops:
       self._openDoors()
+    #checking the stops when going up
     elif self._currentDirection == ElevatorEngine.DIRECTION_UP and self._currentFloor in self._stopsUp:
       self._openDoors()
+    #checking the stops when going down
     elif self._currentDirection == ElevatorEngine.DIRECTION_DOWN and self._currentFloor in self._stopsDown:
       self._openDoors()
+    #checking in the current direction (to avoid going up/down everytime)
     elif self._numberOfStopsInDirection(self._currentDirection) != 0:
       self._goToCurrentDirection()
+    #checking the other direction
     elif self._numberOfStopsInDirection(self._reverseCurrentDirection()) != 0:
       self._goToCurrentReverseDirection()
       
-    ##
+    ##Doing the action
     if self._actions.empty():
       action = ElevatorEngine.ACTION_NOTHING
     else:
       action = self._actions.get()
-    web.debug("action %s at floor %d" % (action, self._currentFloor))
+    web.debug("action: %s" % (action))
     return action
-    
-  def _addStop(self, floor, direction):
-    web.debug("Adding stop to %d in direction %s" % (floor, direction))
-    
-    #If floor is top or bottom, no matter the direction
-    if floor == ElevatorEngine.FLOOR_MAX or floor == 0:
-      direction = ElevatorEngine.DIRECTION_NONE
-    
-    if direction == ElevatorEngine.DIRECTION_UP:
-      self._stopsUp.add(floor)
-    elif direction == ElevatorEngine.DIRECTION_DOWN:
-      self._stopsDown.add(floor)
-    else:
-      self._stops.add(floor)
