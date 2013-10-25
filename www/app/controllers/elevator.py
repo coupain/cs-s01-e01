ffb348e727b5c803a7303fc8eef4087d7e0d87dd
Baptiste Lafontaine <baptiste33@gmail.com>
2013-10-25 20:34:30 +0200

    Moving engine to web module (instead of global)

--- www/app/controllers/elevator.py
+++ www/app/controllers/elevator.py
@@ -1,30 +1,22 @@
 # -*- coding: utf-8 -*-
 
 import web
-from importlib import import_module
-from settings import ENGINE
-#from app.utils.elevator_engine import ElevatorEngine
-
-ElevatorEngine = import_module('app.utils.' + ENGINE).ElevatorEngine
-
-#Using a global for Elevator engine... meh :(
-elevator = ElevatorEngine()
 
 class Elevator(object):
   def GET(self, action):
     params = web.input()
     if action == 'call':
-      elevator.call(int(params.atFloor),params.to)
+      web.elevator.call(int(params.atFloor),params.to)
     elif action == 'go':
-      elevator.go(int(params.floorToGo))
+      web.elevator.go(int(params.floorToGo))
     elif action == 'userHasEntered':
-      elevator.userHasEntered()
+      web.elevator.userHasEntered()
     elif action == 'userHasExited':
-      elevator.userHasExited()
+      web.elevator.userHasExited()
     elif action == 'reset':
         try :
-            elevator.reset(params.cause)
+            web.elevator.reset(params.cause)
         except AttributeError:
-            elevator.reset("")
+            web.elevator.reset("")
     elif action == 'nextCommand':
-      return elevator.getNextCommand()
+      return web.elevator.getNextCommand()
--- www/main.py
+++ www/main.py
@@ -2,12 +2,15 @@
 # -*- coding: utf-8 -*-
 
 import web
-from settings import DEBUG
+import settings
 from urls import URLS
+from importlib import import_module
 
-web.config.debug = DEBUG
+web.config.debug = settings.DEBUG
 
-app = web.application(URLS, globals(), autoreload=DEBUG)
+app = web.application(URLS, globals(), autoreload=settings.DEBUG)
 
 if __name__ == '__main__':
+  ElevatorEngine = import_module('app.utils.' + settings.ENGINE).ElevatorEngine
+  web.elevator = ElevatorEngine()
   app.run()
